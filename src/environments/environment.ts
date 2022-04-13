92574dc88c0b083c1332fec4cdb70e45d41c59ea
joey57 <njorogejoey5@gmail.com>
2022-04-13 16:57:05 +0300

    import bootstrap and apikey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  "apiKey": "mVF9mf4l6R1U8HIMe9TZUc1AoIMb0I3a"
 };
 
 /*
